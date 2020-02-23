251b90c50a60d23346399438608ad3af958798bf
Nik S4 <nikita-s4astlivtsev2012@yandex.ru>
2020-02-23 16:57:14 +0400

    #12 added: - torch textures: switched on and switched off - ignisRegularInner scene - ignisRegularOuter scene - ignisRegularLevel scene

---
+++ IgnisRegularLevel/IgnisRegularLevel.gd
@@ -0,0 +1,27 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var activated = false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func activate():
+	activated = !activated
+	if activated:
+		$SpriteTorchOff.hide()
+		$SpriteTorchOn.show()
+		$Light2D.show() # does it hide light from enemies, too?
+	else:
+		$SpriteTorchOn.hide()
+		$SpriteTorchOff.show()		
+		$Light2D.hide()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
