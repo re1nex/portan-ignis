738f9cc1b6b27c4b2d1b3a2f63d49625a736b3be
Nik S4 <nikita-s4astlivtsev2012@yandex.ru>
2020-02-28 19:07:18 +0400

    #12 added: - collision shape in inner light (for enemies) - handling of the body entered-exited signals in level light fixed: - not hiding but disabling light in the level light

--- IgnisRegularLevel/IgnisRegularLevel.gd
+++ IgnisRegularLevel/IgnisRegularLevel.gd
@@ -4,8 +4,8 @@ extends Area2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var body_informator = null
 var activated = false
-
 
 
 func _init():
@@ -17,20 +17,39 @@ func _ready():
 	activated = false
 	$SpriteTorchOn.hide()
 	$SpriteTorchOff.show()
-	$Light2D.hide()
+	$Light2D.enabled = false	
 	pass # Replace with function body.
 
-func activate():
-	activated = !activated
-	if activated:
-		$SpriteTorchOff.hide()
-		$SpriteTorchOn.show()
-		$Light2D.show() # does it hide light from enemies, too?
-	else:
-		$SpriteTorchOn.hide()
-		$SpriteTorchOff.show()		
-		$Light2D.hide()
 
+func activate():
+	if body_informator != null and body_informator.is_ignis:
+		activated = !activated
+		$Light2D.enabled = !$Light2D.enabled
+		if activated:
+			$SpriteTorchOff.hide()
+			$SpriteTorchOn.show()	
+		else:
+			$SpriteTorchOn.hide()
+			$SpriteTorchOff.show()	
+
+
+func _on_body_exited(body):
+	if body.has_method("get_informator"):
+		if body.get_informator == body_informator:
+			body_informator = null
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_IgnisRegularLevel_body_entered(body):
+	if body.has_method("get_informator"):
+		body_informator = body.get_informator()
+	pass # Replace with function body.
+
+
+func _on_IgnisRegularLevel_body_exited(body):
+	if body.has_method("get_informator"):
+		if body.get_informator() == body_informator:
+			body_informator = null
+	pass # Replace with function body.
