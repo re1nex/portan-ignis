82da219a65fec68a1f5363ccd5129626c13797c2
NikitaS4 <nikita-s4astlivtsev2012@yandex.ru>
2020-03-08 11:59:21 +0300

    #36 added: - light osicillation

--- IgnisRegularInner/IgnisRegularInner.gd
+++ IgnisRegularInner/IgnisRegularInner.gd
@@ -1,5 +1,8 @@
 extends Light2D
 
+const deltaScale = 0.0025
+
+var minScale
 
 signal enabled
 signal disabled
@@ -7,7 +10,12 @@ signal disabled
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	minScale = texture_scale - 0.01
 	pass # Replace with function body.
+
+
+func _process(delta):
+	texture_scale = minScale + float(randf() / (minScale + deltaScale))
 
 
 func disable():
