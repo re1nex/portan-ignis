240de17830edf9c90382aaa8fd15d2f87033ce9a
NikitaS4 <nikita-s4astlivtsev2012@yandex.ru>
2020-03-08 17:26:28 +0300

    #36 fixed: - animation, removed color osicillation

--- IgnisRegularInner/IgnisRegularInner.gd
+++ IgnisRegularInner/IgnisRegularInner.gd
@@ -1,12 +1,8 @@
 extends Light2D
 
 const deltaScale = 0.0025
-const redDeviation = 0.0005
 
 var minScale
-var minRed
-var minGreen
-var minBlue
 
 signal enabled
 signal disabled
@@ -15,23 +11,21 @@ signal disabled
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	minScale = texture_scale - 0.01
-	minRed = color.r - redDeviation / 2
 	pass # Replace with function body.
 
 
 func _process(delta):
 	texture_scale = minScale + float(randf() / (minScale + deltaScale))
-	color.r = minRed + float(randf() / (minRed + redDeviation))
 
 
 func disable():
 	$Area2D.monitorable = false
-	$Particles2D.hide()
+	$Particles2D.emitting = false
 	enabled = false
 	emit_signal("disabled")
 	
 func enable():
-	$Particles2D.show()
+	$Particles2D.emitting = true
 	$Area2D.monitorable = true
 	enabled = true
 	emit_signal("enabled")
