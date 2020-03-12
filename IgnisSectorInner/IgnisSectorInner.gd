a8110e4c123a0d049b499d817ed33226d6087af4
NikitaS4 <nikita-s4astlivtsev2012@yandex.ru>
2020-03-12 23:51:40 +0300

    #37 added: - signals and switching-on/off like in ignisRegularInner

--- IgnisSectorInner/IgnisSectorInner.gd
+++ IgnisSectorInner/IgnisSectorInner.gd
@@ -1,11 +1,40 @@
 extends Light2D
 
+const deltaScale = 0.0025
+const energyDec = 0.025
+const energyMin = 0.1
+
 var reflected = false
 
+var minScale
+var energyMax
+var switchingOff
+var switchedOff
 
-# Called when the node enters the scene tree for the first time.
+signal enabled
+signal disabled
+var priority = 1
+
+
 func _ready():
+	minScale = texture_scale - 0.01
+	energyMax = 1.2
+	switchingOff = not enabled
+	switchedOff = enabled
+	if switchedOff:
+		finishDisabling()
 	pass # Replace with function body.
+
+
+func _process(delta):
+	texture_scale = minScale + float(randf() / (minScale + deltaScale))
+	if switchingOff and not switchedOff:
+		# switching off is in process
+		energy -= energyDec
+		checkEnergy()
+	if not switchingOff and switchedOff:
+		# light needs to be switched on
+		finishEnabling()
 
 
 func mirror():
@@ -33,3 +62,34 @@ func rotate_ignis(val):
 	rotation += val
 	clamp_rotation()
 	pass
+
+
+func checkEnergy():
+	if energy <= energyMin:
+		finishDisabling()
+		switchedOff = true
+
+
+func disable():
+	switchingOff = true
+
+
+func finishDisabling():
+	$Area2D/CollisionShape2D.disabled = true
+	enabled = false
+	energy = 0
+	switchedOff = true
+	emit_signal("disabled")
+
+
+func enable():
+	switchingOff = false
+	energy = energyMax
+
+
+func finishEnabling():
+	switchedOff = false
+	$Area2D/CollisionShape2D.disabled = false
+	enabled = true
+	energy = energyMax
+	emit_signal("enabled")
