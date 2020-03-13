3e2fa38ca1cc9944225c427557f168db03c7b7f0
Victor Kolesnik <55282628+VsevolodMelnikov@users.noreply.github.com>
2020-03-14 00:40:25 +0300

    #44 added:
    - scene-switcher
    - connections between menu, pause-menu and 2 levels
    fixed:
    - shadows, casted by ignises

--- pauseMenu/pauseMenu.gd
+++ pauseMenu/pauseMenu.gd
@@ -1,20 +1,27 @@
 extends MarginContainer
-signal Continue
-signal ExitMenu
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var game_paused = false
+export (bool) var hide_at_start = true
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if hide_at_start:
+		hide()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		game_paused = !game_paused
+		process_pause()
+
+
+func process_pause():
+	if game_paused:
+		get_tree().paused = true
+		show()
+	else:
+		get_tree().paused = false
+		hide()
 
 
 func _on_backSettings_pressed():
@@ -27,7 +34,8 @@ func _on_CheckBox_pressed():
 
 
 func _on_Continue_pressed():
-	emit_signal("Continue")
+	game_paused = false
+	process_pause()
 
 
 func _on_Settings_pressed():
@@ -36,4 +44,6 @@ func _on_Settings_pressed():
 
 
 func _on_MainMenu_pressed():
-	emit_signal("ExitMenu")
+	game_paused = false
+	process_pause()
+	SceneSwitcher.goto_scene(SceneSwitcher.Scenes.SCENE_MAIN_MENU)
---
+++ sceneSwitcher/SceneSwitcher.gd
@@ -0,0 +1,35 @@
+extends Node
+
+enum Scenes{
+	SCENE_STAGE_TEST_DESIGN,
+	SCENE_STAGE_TEST,
+	SCENE_MAIN_MENU,
+}
+
+var current_scene = null
+
+func _ready():
+	var root = get_tree().get_root()
+	current_scene = root.get_child(root.get_child_count() - 1)
+
+
+func goto_scene(scene):
+	call_deferred("_deferred_goto_scene", scene)
+
+
+func _deferred_goto_scene(scene):
+	var new_scene
+	
+	if (scene == Scenes.SCENE_STAGE_TEST_DESIGN):
+		new_scene = ResourceLoader.load("res://levelTileSet/TileMap.tscn")
+	elif (scene == Scenes.SCENE_STAGE_TEST):
+		new_scene = ResourceLoader.load("res://Main/Main.tscn")
+	elif (scene == Scenes.SCENE_MAIN_MENU):
+		new_scene = ResourceLoader.load("res://mainMenu/MainMenuView.tscn")
+	else:
+		return
+	
+	current_scene.free()
+	current_scene = new_scene.instance()
+	get_tree().get_root().add_child(current_scene)
+	get_tree().set_current_scene(current_scene)
