3fbecc5b618c7ca20803b13deb14bfff5753c0cc
NikitaS4 <nikita-s4astlivtsev2012@yandex.ru>
2020-03-24 13:55:04 +0300

    #93 fixed: - progress bar fading

--- src/ui/HUD/HUD.gd
+++ src/ui/HUD/HUD.gd
@@ -3,13 +3,17 @@ extends MarginContainer
 
 const InformatorClass = preload("res://src/characters/player/Informator.gd")
 const PlayerClass = preload("res://src/characters/player/Player.tscn")
-
+const fading_delta = 0.1
 
 var informator
 var health
 var torch_lives
 var player
-
+var old_status
+var progress_bar_fading
+var fading_in
+var max_alpha
+var min_alpha
 
 func _ready():
 	pass
@@ -20,13 +24,23 @@ func init_player(plr):
 	informator = player.get_informator()
 	set_health_bar(informator.health)	
 	set_ignis_bar(informator.ignis_timer_start)
+	init_ignis_bar_anim()
+
+
+func init_ignis_bar_anim():
+	old_status = informator.ignis_status
+	progress_bar_fading = false
+	fading_in = false 
+	max_alpha = $MainContainer/Bars.modulate.a
+	min_alpha = 0
 
 
 func _process(delta):
 	set_health_bar(informator.health)
 	set_ignis_bar(informator.ignis_timer_start)
-	upd_ignis_bar()
+	upd_ignis_bar(informator.ignis_status)
 	upd_chosen_ignis(informator.num_of_active_weapon)
+	ignis_bar_anim()
 
 
 func upd_chosen_ignis(active_weapon):
@@ -43,11 +57,31 @@ func upd_chosen_ignis(active_weapon):
 	pass
 
 
-func upd_ignis_bar():
-	if informator.ignis_status == informator.Is_ignis.NO_IGNIS:
-		$MainContainer/Bars/Bar/torchStatus.hide()
+func upd_ignis_bar(ignis_status):
+	if ignis_status == old_status:
+		return
+	# status changed
+	old_status = ignis_status # update status
+	match ignis_status:
+		informator.Is_ignis.NO_IGNIS:
+			progress_bar_fading = true # to start animation
+			fading_in = false # fading out
+		_:
+			progress_bar_fading = true # to start animation
+			fading_in = true # fading in
+
+
+func ignis_bar_anim():
+	if not progress_bar_fading:
+		return
+	if fading_in:
+		$MainContainer/Bars.modulate.a += fading_delta
+		if $MainContainer/Bars.modulate.a >= max_alpha:
+			progress_bar_fading = false # stop animation
 	else:
-		$MainContainer/Bars/Bar/torchStatus.show()
+		$MainContainer/Bars.modulate.a -= fading_delta
+		if $MainContainer/Bars.modulate.a <= min_alpha:
+			progress_bar_fading = false # stop animation
 
 
 func set_ignis_bar(value):
