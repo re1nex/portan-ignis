11e10b6d389c45ee13399d6d6da7ff3fd49a76a8
Aleksandr Mitenev <aleksandr.mitenev@mail.ru>
2020-03-29 14:16:13 +0300

    #109 fixed: - doors movement

--- src/objects/door/Door.gd
+++ src/objects/door/Door.gd
@@ -1,22 +1,39 @@
 extends StaticBody2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var height
+var step
+var max_height
+const SPEED = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	height = 0
+	step = 0
+	max_height = $CollisionShape2D.shape.extents.y * 2
 	pass # Replace with function body.
 
+func _process(delta):
+	if step != 0:
+		var del = step * delta
+		position.y += del
+		height -= del
+		if height > max_height:
+			position.y += height - max_height
+			height -= height - max_height
+			step = 0
+		elif height < 0:
+			position.y += height
+			height = 0
+			step = 0
+	pass
+
+
 func _on_IgnisRegularLevel_active():
-	set_collision_layer_bit(0, false)
-	$Sprite.hide()
+	step = -SPEED
 	pass # Replace with function body.
 
 
 func _on_IgnisRegularLevel_not_active():
-	$Sprite.show()
-	set_collision_layer_bit(0, true)
+	step = SPEED
 	pass # Replace with function body.
