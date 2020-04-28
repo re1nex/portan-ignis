58b781e47d10fed901599d67749c3ce355a8378e
Elisey Vasilevskiy <48808317+re1nex@users.noreply.github.com>
2020-04-28 23:15:57 +0300

    #237 added: - music controller of sound - scroll controller by keyboard

--- src/globals/settings/AudioController.gd
+++ src/globals/settings/AudioController.gd
@@ -3,8 +3,7 @@ extends Node
 
 func changeVol(value):
 	if(value==0):
-		Settings.Sound["Mute"] = true
-		AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), true)
+		turnVol(false)
 		return
 	Settings.Sound["Mute"] = false
 	AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), false)
@@ -17,6 +16,18 @@ func turnVol(on):
 	if(on):
 		Settings.Sound["Mute"] = false
 		AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), false)
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("Music"), false)
 	else:
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("Music"), true)
 		AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), true)
 		Settings.Sound["Mute"] = true
+
+
+func changeMusicVol(value):
+	if(value==0):
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("Music"), true)
+		return
+	AudioServer.set_bus_mute(AudioServer.get_bus_index("Music"), false)
+	var dlt=(Settings.Sound["MusicVol"]-value)/4
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"),AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Music"))- dlt)
+	Settings.Sound["MusicVol"]=value
