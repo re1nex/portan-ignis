ee2edec0a827c4a19e91150e26e7ef89190396e8
NikitaS4 <nikita-s4astlivtsev2012@yandex.ru>
2020-05-05 16:49:06 +0300

    #253 added: - texture of fuel - method to take fuel in player fixed: - fuel script - fuel scene (sizes of shapes) - fuel node flags

--- src/objects/fuel/Fuel.gd
+++ src/objects/fuel/Fuel.gd
@@ -1,16 +1,23 @@
 extends Area2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var amplitude = 5
+var pos = 0
+var step = 20
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if not $VisibilityEnabler2D.is_on_screen():
+		set_process(false)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	pos += step * delta
+	position.y += step * delta
+	if abs(pos) >= amplitude:
+		step *= -1 
+
+
+func _on_Fuel_body_entered(body):
+	if body.has_method("take_fuel"):
+		if body.take_fuel():
+			queue_free()
