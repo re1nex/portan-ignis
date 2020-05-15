f50de06289d2fceb393003516dd166435ea142ce
NikitaS4 <nikita-s4astlivtsev2012@yandex.ru>
2020-05-15 12:03:26 +0300

    #305 added: - files to store text - script that gives text in chosen language

---
+++ src/globals/textStorage/textStorage.gd
@@ -0,0 +1,43 @@
+extends Node
+
+
+const res_path = "res://resource/textStorage/"
+const default_lang = GlobalVars.User_lang.ENGLISH
+const _file_names = {
+	GlobalVars.Storage_string_id.MENU: res_path + "menu.cfg",
+	GlobalVars.Storage_string_id.HINT: res_path + "hint.cfg",
+}
+const load_error_string = {
+	GlobalVars.User_lang.ENGLISH: "[Text not found]",
+	GlobalVars.User_lang.RUSSIAN: "[Текст не найден]",
+}
+const lang_prop = {
+	GlobalVars.User_lang.ENGLISH: "Eng",
+	GlobalVars.User_lang.RUSSIAN: "Rus",
+}
+
+var _lang = default_lang
+var current_res_path
+var _storage_paths = []
+var _loaded_resources = {
+	GlobalVars.Storage_string_id.MENU: ConfigFile.new(),
+	GlobalVars.Storage_string_id.HINT: ConfigFile.new(),
+}
+var is_loaded = false
+
+
+# storage is GlobalVars.Storage_string_id enum
+func get_string(storage_enum, str_id):
+	if not is_loaded: # resource has not been loaded yet
+		set_lang(_lang)
+	var val = _loaded_resources[storage_enum].get_value(str_id, lang_prop[_lang], load_error_string[_lang])
+
+
+func set_lang(to_lang):
+	_lang = to_lang
+	_load_text()
+
+
+func _load_text():
+	for cur_prop in GlobalVars.Storage_string_id.values():
+		_loaded_resources[cur_prop].load(_storage_paths[cur_prop])
